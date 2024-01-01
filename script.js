071aee6c83468c56ff5dd3414ecd36dc6ee85184
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-01 15:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function fibonacci(num) {
+
 // your code here
+	function fibonacci(num) {
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 0; // First term is considered 0 in the sequence
+  } else if (num === 2) {
+    return 1; // Second term is considered 1 in the sequence
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
+
+
 
 module.exports = fibonacci;
