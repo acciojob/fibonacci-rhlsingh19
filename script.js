32991cbf27c1ea5fbe6ed3c199820c2b1d2ef71c
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-01 15:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-
-// your code here
-	function fibonacci(num) {
-  if (num <= 0) {
+function fibonacci(num) {
+  if (num === 0) {
     return 0;
   } else if (num === 1) {
-    return 0; // First term is considered 0 in the sequence
-  } else if (num === 2) {
-    return 1; // Second term is considered 1 in the sequence
+    return 1;
   } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
+    let fibArray = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fibArray[num];
   }
 }
 
+// Example usage:
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
 
 
 module.exports = fibonacci;
